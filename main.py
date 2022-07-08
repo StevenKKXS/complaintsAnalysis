d69f66dc97e0225435c05ab780446fb3c7ffd9d1
StevenKKXS <3537454479@qq.com>
2022-07-08 15:14:18 +0800

    First commit

---
+++ main.py
@@ -0,0 +1,16 @@
+
+import os.path as osp
+import re
+
+import jieba as jb
+import pandas as pd
+
+
+def split_by_punc(str_in):
+    return str(str_in).split('，')
+
+
+if __name__ == '__main__':
+   pass
+
+
