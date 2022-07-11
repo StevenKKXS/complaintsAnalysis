1b78e58a25c2e2facd3fbbe483bf95abbabe72ca
StevenKKXS <3537454479@qq.com>
2022-07-11 16:42:40 +0800

    switch off gpu for LSTM in keras

--- LSTM_keras.py
+++ LSTM_keras.py
@@ -1,3 +1,9 @@
+import os
+
+# Let keras model to run on cpu
+os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"
+os.environ["CUDA_VISIBLE_DEVICES"] = "-1"
+
 import keras.models
 import pandas as pd
 import matplotlib
@@ -10,13 +16,15 @@ plt.rcParams['axes.unicode_minus'] = False
 from keras.preprocessing.text import Tokenizer
 from keras_preprocessing.sequence import pad_sequences
 from keras.models import Sequential
-from keras.layers import Dense, Embedding, LSTM, SpatialDropout1D
+from keras.layers import Dense, Embedding, LSTM, SpatialDropout1D, CuDNNLSTM
 from sklearn.model_selection import train_test_split
 from keras.utils.np_utils import to_categorical
 from keras.callbacks import EarlyStopping
 from keras.layers import Dropout
+import tensorflow as tf
 import jieba as jb
 import re
+import pickle
 import os.path as osp
 
 from data_process import *
@@ -41,7 +49,23 @@ def onehot_encoder(labels):
     return label_onehot, label_map
 
 
-def train_and_eval(model_path='models/LSTM', random_state=42):
+def get_model(max_nb_words=50000, max_seq_len=150, embedding_dim=32, classification_num=20):
+    tokenizer = Tokenizer(num_words=max_nb_words, filters='!"#$%&()*+,-./:;<=>?@[\]^_`{|}~', lower=True)
+    model = Sequential()
+    model.add(Embedding(max_nb_words, embedding_dim, input_length=max_seq_len, mask_zero=True))
+    model.add(SpatialDropout1D(0.2))
+    model.add(LSTM(embedding_dim, dropout=0.2, recurrent_dropout=0.2, activation='tanh'))
+    model.add(Dense(classification_num, activation='softmax'))
+    model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+    print(model.summary())
+    model_zip = {'model': model, 'tokenizer': tokenizer}
+    return model_zip
+
+
+def main():
+    model_path = 'models/LSTM'
+    random_state = 42
+
     # load data
     train_label_list, train_text_list, val_label_list, val_text_list, test_label_list, test_text_list, class_table = get_data(
         1)
@@ -51,14 +75,21 @@ def train_and_eval(model_path='models/LSTM', random_state=42):
 
     Y, label_map = onehot_encoder(label_list)
 
-    # 设置最频繁使用的50000个词
-    MAX_NB_WORDS = 50000
+    # 设置最频繁使用的25000个词
+    MAX_NB_WORDS = 25000
     # 每条cut_review最大的长度
     MAX_SEQUENCE_LENGTH = 150
     # 设置Embeddingceng层的维度
     EMBEDDING_DIM = 32
 
-    tokenizer = Tokenizer(num_words=MAX_NB_WORDS, filters='!"#$%&()*+,-./:;<=>?@[\]^_`{|}~', lower=True)
+    # get model
+    model_zip = get_model(max_nb_words=MAX_NB_WORDS,
+                          max_seq_len=MAX_SEQUENCE_LENGTH,
+                          embedding_dim=EMBEDDING_DIM,
+                          classification_num=len(label_map))
+
+    model, tokenizer = model_zip.values()
+
     tokenizer.fit_on_texts(text_list)
     word_index = tokenizer.word_index
     print('共有 %s 个不相同的词语.' % len(word_index))
@@ -73,40 +104,58 @@ def train_and_eval(model_path='models/LSTM', random_state=42):
 
     # 拆分训练集和测试集
     X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.10, random_state=random_state)
-    print(X_train.shape, Y_train.shape)
-    print(X_test.shape, Y_test.shape)
-
-    model = Sequential()
-    model.add(Embedding(MAX_NB_WORDS, EMBEDDING_DIM, input_length=X.shape[1], mask_zero=True))
-    model.add(SpatialDropout1D(0.2))
-    model.add(LSTM(EMBEDDING_DIM, dropout=0.2, recurrent_dropout=0.2, activation='tanh'))
-    model.add(Dense(len(label_map), activation='softmax'))
-    model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
-    print(model.summary())
 
     epochs = 5
     batch_size = 64
 
-    history = model.fit(X_train, Y_train, epochs=epochs, batch_size=batch_size, validation_split=0.1,
-                        callbacks=[EarlyStopping(monitor='val_loss', patience=3, min_delta=0.0001)])
+    model, history = train(model, X_train, Y_train, epochs=epochs, batch_size=batch_size)
 
-    accr = model.evaluate(X_test, Y_test)
-    print('Test set\n  Loss: {:0.3f}\n  Accuracy: {:0.3f}'.format(accr[0], accr[1]))
+    loss, acc = evaluate(model, X_test, Y_test)
+    print('Test set\n  Loss: {:0.3f}\n  Accuracy: {:0.3f}'.format(loss, acc))
 
+    model_zip = {'model': model, 'tokenizer': tokenizer, 'history': history}
     # save model
-    model.save(osp.join(model_path, 'model'))
-    np.save(osp.join(model_path, 'word_index.npy'), word_index, allow_pickle=True)
+    save_model(model_path, model_zip)
 
-    return model, word_index
+
+def train(model, train_set, train_label, epochs=5, batch_size=64):
+    history = model.fit(train_set, train_label, epochs=epochs, batch_size=batch_size, validation_split=0.1,
+                        callbacks=[EarlyStopping(monitor='val_loss', patience=3, min_delta=0.0001)])
+    return model, history
+
+
+def evaluate(model, test_set, test_label):
+    accr = model.evaluate(test_set, test_label)
+    loss = accr[0]
+    acc = accr[1]
+    return loss, acc
+
+
+def save_model(save_path, model_zip):
+    if not osp.exists(save_path):
+        os.makedirs(save_path)
+    model = model_zip.get('model')
+    tokenizer = model_zip.get('tokenizer')
+    history = model_zip.get(tokenizer)
+    model.save(osp.join(save_path, 'model'))
+    with open(osp.join(save_path, 'tokenizer.pickle'), 'wb') as handle:
+        pickle.dump(tokenizer, handle, protocol=pickle.HIGHEST_PROTOCOL)
+    with open(osp.join(save_path, 'history.pickle'), 'wb') as handle:
+        pickle.dump(history, handle, protocol=pickle.HIGHEST_PROTOCOL)
 
 
 def load_model(model_path):
     model = keras.models.load_model(osp.join(model_path, 'model'))
-    word_index = np.load(osp.join(model_path, 'word_index.npy'), allow_pickle=True).item()
-    return model, word_index
+    with open(osp.join(model_path, 'tokenizer.pickle'), 'rb') as handle:
+        tokenizer = pickle.load(handle)
+    with open(osp.join(model_path, 'history.pickle'), 'rb') as handle:
+        history = pickle.load(handle)
+    model_zip = {'model': model, 'tokenizer': tokenizer, 'history': history}
+    return model_zip
 
 
-def tsne_visualization(model, word_index):
+def tsne_visualization(model, tokenizer):
+    word_index = tokenizer.word_index
     index_word = dict(zip(word_index.values(), word_index.keys()))
     # model.summary()
     embedding_weights = model.get_layer("embedding").get_weights()[0]
@@ -169,6 +218,6 @@ def draw_dataset_len_histogram(bins=10, max_len=None):
 
 
 if __name__ == "__main__":
-    # train_and_eval('models/LSTM_zero_mask')
-    m, wi = load_model('models/LSTM_zero_mask')
-    tsne_visualization(m, wi)
+    main()
+    # m, token = load_model('models/LSTM_zero_mask')
+    # tsne_visualization(m, token)
